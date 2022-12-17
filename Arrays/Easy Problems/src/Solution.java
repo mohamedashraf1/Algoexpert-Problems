8be0f1d1aae6c622a3a3152d82870f25a91443fb
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-12-17 20:19:49 +0200

    the solution

--- Arrays/Easy Problems/src/Solution.java
+++ Arrays/Easy Problems/src/Solution.java
@@ -1,4 +1,33 @@
+import java.util.Hashtable;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Solution {
+
+
+    public static int[] twoNumSum(int [] arr, int target){
+        Hashtable<Integer, Integer> myMap = new Hashtable<>();
+        // adding the array to a hashtable
+        for (int i = 0; i < arr.length; i++) {
+            myMap.put(arr[i], arr[i]);
+        }
+
+        int result [] = new int[2];
+        // getting the set of keys from the map to loop through it
+        Set<Integer> setOfKeys = myMap.keySet();
+
+        // subtract the key from the target and search for it in the hashtable
+        for (Integer key: setOfKeys) {
+            int left = target - key;
+            if(myMap.containsKey(left)){
+                result[0] = key;
+                result[1] = left;
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
