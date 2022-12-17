a7b2e52ff644606427b1daed193cce09ccc61559
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-12-17 20:15:37 +0200

    taking input from the user and printing the output

--- Arrays/Easy Problems/src/Solution.java
+++ Arrays/Easy Problems/src/Solution.java
@@ -1,5 +1,20 @@
 public class Solution {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter array : ");
+        String line = sc.nextLine();
+        String[] tokens = line.split(" ");
+        int[] numbers = new int[tokens.length];
+        for (int i=0; i<numbers.length;i++){
+            numbers[i] = Integer.parseInt(tokens[i]);
+        }
+        System.out.println("Enter Target : ");
+        int target = sc.nextInt();
+
+        int result [] = twoNumSum(numbers, target);
+
+        System.out.print("["+result[0]+" , " + result[1]+"]");
+
     }
 }
